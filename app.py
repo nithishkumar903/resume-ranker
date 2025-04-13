8a241394707d26abfbc19105b2a6f0bed19af64e
nithishkumar903 <164052232+nithishkumar903@users.noreply.github.com>
2025-04-13 12:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import streamlit as st
-from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from PyPDF2 import PdfReader
 import docx2txt
